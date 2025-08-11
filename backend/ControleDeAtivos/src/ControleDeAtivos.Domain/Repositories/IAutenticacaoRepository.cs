f3b919fc8975ddeafbf2facacdee8eecdb9e3c07
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-11 16:20:55 -0300

    feat(domain): Adicionando as interfaces de repository

---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.Domain/Repositories/IAutenticacaoRepository.cs
@@ -0,0 +1,21 @@
+﻿using ControleDeAtivos.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAtivos.Domain.Repositories
+{
+    public interface IAutenticacaoRepository
+    {
+        Task<Usuario?> ValidarCredenciaisAsync(string login);
+        Task<Usuario?> ObterUsuarioPorIdAsync(int usuarioId);
+
+        Task AdicionarRefreshTokenAsync(RefreshToken token);
+        Task<RefreshToken?> ObterRefreshTokenAsync(string token);
+        Task AtualizarRefreshTokenAsync(RefreshToken token);
+        Task SalvarAlteracoesAsync();
+    }
+
+}
---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.Domain/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using ControleDeAtivos.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAtivos.Domain.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        Task<List<Usuario>> ObterTodosAsync();
+        Task<Usuario> ObterPorIdAsync(int id);
+        Task AdicionarAsync(Usuario usuario);
+        Task RemoverAsync(Usuario usuario);
+        Task AtualizarAsync(Usuario usuario);
+        Task SalvarAsync();
+    }
+}
