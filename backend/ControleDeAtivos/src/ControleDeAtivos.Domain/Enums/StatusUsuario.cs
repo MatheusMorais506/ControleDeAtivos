a0f34501b5554edffde527797334ca38feeea928
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-11 16:19:46 -0300

    feat(domain): Adicionando enums para perfil e status de usuario

---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.Domain/Enums/StatusUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAtivos.Domain.Enums
+{
+    public enum StatusUsuario
+    {
+        Ativo = 1,
+        Inativo = 2
+    }
+}
