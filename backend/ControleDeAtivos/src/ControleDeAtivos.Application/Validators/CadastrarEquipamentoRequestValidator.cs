370b11c24c87d6ea05f2fbcc22f1ee44eaa21afb
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-11 16:57:18 -0300

    feat(application): Adicionando validação FluentValidation para as classes de cadastro

---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.Application/Validators/CadastrarEquipamentoRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using ControleDeAtivos.Application.Requests.Equipamento;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAtivos.Application.Validators
+{
+    public class CadastrarEquipamentoRequestValidator : AbstractValidator<RequestCadastrarEquipamentoJson>
+    {
+        public CadastrarEquipamentoRequestValidator()
+        {
+            RuleFor(x => x.Nome).NotEmpty().WithMessage("Nome é obrigatório");
+            RuleFor(x => x.CodigoIdentificacao).NotEmpty().WithMessage("Código é obrigatório");
+        }
+    }
+}
---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.Application/Validators/CadastrarUsuarioRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using ControleDeAtivos.Application.Responses.Usuario;
+using FluentValidation;
+
+namespace ControleDeAtivos.Application.Validators
+{
+    public class CadastrarUsuarioRequestValidator : AbstractValidator<ResponseCadastrarUsuarioJson>
+    {
+        public CadastrarUsuarioRequestValidator()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("Nome é obrigatório");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email é obrigatório")
+                .EmailAddress().WithMessage("Email inválido");
+
+            RuleFor(x => x.Login)
+                .NotEmpty().WithMessage("Login é obrigatório")
+                .MinimumLength(3).WithMessage("Login deve ter pelo menos 3 caracteres")
+                .MaximumLength(20).WithMessage("Login deve ter no máximo 20 caracteres");
+
+            RuleFor(x => x.Status)
+                .NotNull().WithMessage("Status é obrigatório");
+
+            RuleFor(x => x.Perfil)
+                .NotNull().WithMessage("Perfil é obrigatório");
+        }
+    }
+}
