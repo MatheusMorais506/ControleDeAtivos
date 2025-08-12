b20873846d6889653e304a1e7efe70a8183ef979
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-12 08:28:30 -0300

    feat(api): Adicionando a classe de validacao para o FluentValidation

---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.API/Filters/FluentValidationFilter.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ControleDeAtivos.Api.Filters
+{
+    public class FluentValidationFilter : IActionFilter
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public FluentValidationFilter(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            foreach (var argument in context.ActionArguments.Values)
+            {
+                if (argument == null) continue;
+
+                var validatorType = typeof(IValidator<>).MakeGenericType(argument.GetType());
+                var validator = _serviceProvider.GetService(validatorType) as IValidator;
+
+                if (validator != null)
+                {
+                    var validationResult = validator.Validate(new ValidationContext<object>(argument));
+                    if (!validationResult.IsValid)
+                    {
+                        foreach (var error in validationResult.Errors)
+                        {
+                            context.ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                        }
+                        context.Result = new BadRequestObjectResult(context.ModelState);
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        
+        }
+    }
+
+}
