de3a43ef5a7acc3764fb332e3e372d8687b14690
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-12 08:27:48 -0300

    feat(api): Adicionando o controller de equipamentos

---
+++ backend/ControleDeAtivos/src/ControleDeAtivos.API/Controllers/EquipamentosController.cs
@@ -0,0 +1,66 @@
+﻿using ControleDeAtivos.Application.Interfaces.Equipamentos;
+using ControleDeAtivos.Application.Requests.Equipamento;
+using ControleDeAtivos.Application.Responses.Equipamento;
+using ControleDeAtivos.Application.services.Equipamentos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeAtivos.Api.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipamentosController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseEquipamentoJson), StatusCodes.Status201Created)]
+        public async Task<IActionResult> Cadastrar(
+       [FromServices] ICadastrarEquipamentoService service,
+       [FromBody] RequestCadastrarEquipamentoJson request)
+        {
+            var result = await service.CadastrarAsync(request.Nome, request.CodigoIdentificacao);
+            return CreatedAtAction(nameof(Cadastrar), new { id = result.Id }, result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<ResponseEquipamentoJson>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Listar(
+            [FromServices] IListarEquipamentoService service)
+        {
+            var result = await service.ListarAsync();
+            return Ok(result);
+        }
+
+        [HttpPut("{id}/emprestar")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Emprestar(
+            int id,
+            [FromServices] IEmprestarEquipamentoService service,
+            [FromBody] RequestEmprestarEquipamentoJson request)
+        {
+            await service.EmprestarAsync(id, request.Nota);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/devolver")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Devolver(
+            int id,
+            [FromServices] IDevolverEquipamentoService service)
+        {
+            await service.DevolverAsync(id);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Remover(
+            int id,
+            [FromServices] IRemoverEquipamentoService service)
+        {
+            await service.RemoverAsync(id);
+            return NoContent();
+        }
+    }
+}
