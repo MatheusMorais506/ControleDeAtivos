9811d40f96de0e8146ab96d8328871ee3e929c29
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-12 08:32:10 -0300

    chore(api): Incluindo as referencias, injecao de dependencias, cors e token jwt

--- backend/ControleDeAtivos/src/ControleDeAtivos.API/Program.cs
+++ backend/ControleDeAtivos/src/ControleDeAtivos.API/Program.cs
@@ -1,11 +1,130 @@
+using ControleDeAtivos.Api.Filters;
+using ControleDeAtivos.Application.Interfaces.Autenticacao;
+using ControleDeAtivos.Application.Interfaces.Equipamentos;
+using ControleDeAtivos.Application.Interfaces.Token;
+using ControleDeAtivos.Application.Interfaces.Usuarios;
+using ControleDeAtivos.Application.services.Autenticacao;
+using ControleDeAtivos.Application.services.Equipamentos;
+using ControleDeAtivos.Application.services.Usuarios;
+using ControleDeAtivos.Application.Validators;
+using ControleDeAtivos.Domain.Repositories;
+using ControleDeAtivos.Infraestructure.Security;
+using ControleDeAtivos.Infrastructure.Data;
+using ControleDeAtivos.Infrastructure.Repositories;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+var contentRoot = builder.Environment.ContentRootPath;
+var solutionRoot = Path.GetFullPath(Path.Combine(contentRoot, "..", ".."));
+var dbPath = Path.Combine(solutionRoot, "database", "ControleDeAtivos.db");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+
+builder.Services.AddScoped<IEquipamentoRepository, EquipamentoRepository>();
+builder.Services.AddScoped<ICadastrarEquipamentoService, CadastrarEquipamentoservice>();
+builder.Services.AddScoped<IListarEquipamentoService, ListarEquipamentoservice>();
+builder.Services.AddScoped<IEmprestarEquipamentoService, EmprestarEquipamentoservice>();
+builder.Services.AddScoped<IDevolverEquipamentoService, DevolverEquipamentoservice>();
+builder.Services.AddScoped<IRemoverEquipamentoService, RemoverEquipamentoservice>();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<ICadastrarUsuarioService, CadastrarUsuarioservice>();
+builder.Services.AddScoped<IAtualizarUsuarioService, AtualizarUsuarioservice>();
+builder.Services.AddScoped<IConsultarUsuarioService, ConsultarUsuariosservice>();
+builder.Services.AddScoped<IRemoverUsuarioService, RemoverUsuarioservice>();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddScoped<IAutenticacaoRepository, AutenticacaoRepository>();
+builder.Services.AddScoped<ILoginService, Loginservice>();
+builder.Services.AddScoped<ILogoutService, Logoutservice>();
+builder.Services.AddScoped<IRefreshService, RefreshTokenservice>();
+
+var allowedOrigins = new string[]
+{
+    "http://localhost:3000",
+};
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+builder.Services.AddControllers()
+    .AddMvcOptions(options =>
+    {
+        options.Filters.Add(typeof(FluentValidationFilter));
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Controle de Ativos", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o token JWT no campo abaixo. Exemplo: 'Bearer {seu_token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddValidatorsFromAssemblyContaining<CadastrarEquipamentoRequestValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CadastrarUsuarioRequestValidator>();
+
+
+var key = builder.Configuration["Jwt:ChaveSecreta"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +137,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowSpecificOrigins");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
