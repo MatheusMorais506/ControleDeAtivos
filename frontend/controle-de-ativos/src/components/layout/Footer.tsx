7942d9f5568eb3c0668dea8dce382df1e8737efb
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-13 13:55:00 -0300

    refactor: refatorando componentes de layout

--- frontend/controle-de-ativos/src/components/layout/Footer.tsx
+++ frontend/controle-de-ativos/src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-export const Footer: React.FC = () => {
+export function Footer()  {
   return (
    <footer className="bg-gray-100 border-t border-gray-200 py-2 text-center text-sm text-gray-500 w-full">
       © {new Date().getFullYear()} Controle de Ativos. Todos os direitos reservados.
--- frontend/controle-de-ativos/src/components/layout/Navbar.tsx
+++ frontend/controle-de-ativos/src/components/layout/Navbar.tsx
@@ -1,14 +1,11 @@
 'use client';
-import { AuthGuard } from '@/services/authGuard';
+import { AutenticacaoGuard } from '@/guards/AutenticacaoGuard';
+import { NavbarProps } from '@/types/NavbarProps';
 import { FaBars } from 'react-icons/fa';
 
-interface NavbarProps {
-  toggleSidebar: () => void;
-}
-
-export const Navbar: React.FC<NavbarProps> = ({ toggleSidebar }) => {
+export function Navbar({ toggleSidebar }: NavbarProps) {
   return (
-    <AuthGuard>
+    <AutenticacaoGuard>
       <header className="bg-white border-b border-gray-200 px-4 py-2 flex items-center justify-between">
         <button
           onClick={toggleSidebar}
@@ -19,6 +16,6 @@ export const Navbar: React.FC<NavbarProps> = ({ toggleSidebar }) => {
         <h1 className="font-bold text-gray-700"></h1>
         <div></div>
       </header>
-    </AuthGuard>
+    </AutenticacaoGuard>
   );
 };
