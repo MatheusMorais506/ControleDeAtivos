1be87790955a26f9028beefa1655e79d2fcf437d
MatheusMorais506 <matheus.moraes09@hotmail.com>
2025-08-12 23:26:52 -0300

    feat: Incluindo lib para mensagens de sucesso e erro

---
+++ frontend/controle-de-ativos/src/lib/toastLib.tsx
@@ -0,0 +1,39 @@
+// toastLib.tsx
+import React from 'react';
+import { ToastContainer, toast, ToastOptions } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const defaultOptions: ToastOptions = {
+  position: "top-right",
+  autoClose: 4000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
+export function ToastProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <ToastContainer {...defaultOptions} />
+    </>
+  );
+}
+
+export const showSuccess = (message: string, options?: ToastOptions) => {
+  toast.success(message, { ...defaultOptions, ...options });
+};
+
+export const showError = (message: string, options?: ToastOptions) => {
+  toast.error(message, { ...defaultOptions, ...options });
+};
+
+export const showWarning = (message: string, options?: ToastOptions) => {
+  toast.warning(message, { ...defaultOptions, ...options });
+};
+
+export const showInfo = (message: string, options?: ToastOptions) => {
+  toast.info(message, { ...defaultOptions, ...options });
+};
