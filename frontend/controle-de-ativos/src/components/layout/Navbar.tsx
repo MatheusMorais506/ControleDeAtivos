ec7a8b0ee24a9a2b7b1a82f6843ac83e286bb348
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-13 15:10:19 -0300

    feat: Incluindo nome do usuario logado no navbar

--- frontend/controle-de-ativos/src/components/layout/Navbar.tsx
+++ frontend/controle-de-ativos/src/components/layout/Navbar.tsx
@@ -1,9 +1,12 @@
 'use client';
+import { useAuth } from '@/context/AutenticacaoContext';
 import { AutenticacaoGuard } from '@/guards/AutenticacaoGuard';
 import { NavbarProps } from '@/types/NavbarProps';
 import { FaBars } from 'react-icons/fa';
 
 export function Navbar({ toggleSidebar }: NavbarProps) {
+  const { user } = useAuth();
+  
   return (
     <AutenticacaoGuard>
       <header className="bg-white border-b border-gray-200 px-4 py-2 flex items-center justify-between">
@@ -13,8 +16,10 @@ export function Navbar({ toggleSidebar }: NavbarProps) {
         >
           <FaBars size={20} />
         </button>
-        <h1 className="font-bold text-gray-700"></h1>
-        <div></div>
+        <h1 className="font-bold text-gray-700 "></h1>
+        <div className="text-gray-700 font-medium text-sm">
+          {user ? `Olá, ${user.nome}` : ""}
+        </div>
       </header>
     </AutenticacaoGuard>
   );
