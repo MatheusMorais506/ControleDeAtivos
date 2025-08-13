1abbac0a69450dd883c7270a10a1902605c90c62
Matheus Silva <matheus.moraes09@hotmail.com>
2025-08-13 16:28:08 -0250

    refactor: refatorando o código

--- frontend/controle-de-ativos/src/components/layout/Footer.tsx
+++ frontend/controle-de-ativos/src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-100 border-t border-gray-200 py-2 text-center text-sm text-gray-500">
+   <footer className="bg-gray-100 border-t border-gray-200 py-2 text-center text-sm text-gray-500 w-full">
       © {new Date().getFullYear()} Controle de Ativos. Todos os direitos reservados.
     </footer>
   );
